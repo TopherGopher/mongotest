23908e2f37ef3022e7b9c63a35bbe6abe48d8cab
Topher Sterling <topher@develops.guru>
2021-08-27 00:09:04 -0600

    Refactor this a little to be cleaner.

--- mongotest.go
+++ mongotest.go
@@ -1,8 +1,9 @@
 // Package mongotest provides helpers for running regressions using mongo.
 // You can find helpers for:
-// - ?running a database using docker
-// - importing data from files
-// - cleaning up a database
+// - running a database using docker
+// - TODO: importing data to the DB from files
+// - TODO: exporting data from the DB to a file
+// - TODO: cleaning up a database
 package mongotest
 
 import (
@@ -10,6 +11,7 @@ import (
 	"fmt"
 	"io/ioutil"
 	"net"
+	"os"
 	"runtime/debug"
 	"strconv"
 	"time"
@@ -17,18 +19,59 @@ import (
 	"docker.io/go-docker"
 	"docker.io/go-docker/api/types"
 	"docker.io/go-docker/api/types/container"
+	"docker.io/go-docker/api/types/mount"
 	"docker.io/go-docker/api/types/network"
 	"github.com/docker/go-connections/nat"
 	"github.com/sirupsen/logrus"
 	"github.com/tophergopher/easymongo"
+	"go.mongodb.org/mongo-driver/mongo"
 )
 
 // TestConnection contains helpers for creating your own tests with mongo.
+// Each TestConnection corresponds 1-to-1 with a docker container.
+// Each docker container is hosted on a unique port.
 type TestConnection struct {
 	*easymongo.Connection
 	dockerClient     *docker.Client
 	logger           *logrus.Entry
 	mongoContainerID string
+	caPemFile        *os.File
+	portNumber       int
+	mongoURI         string
+	mongoVersion     string
+}
+
+// initDocker initializes the various docker components we need
+// It must be called before interacting with any docker components
+func (testConn *TestConnection) initDocker() error {
+	dockerClient, err := docker.NewEnvClient()
+	if err != nil {
+		testConn.logger.WithField("err", err).Error("Could not connect to docker daemon")
+		return ErrFailedToConnectToDockerDaemon
+	}
+	testConn.dockerClient = dockerClient
+	return nil
+}
+
+// spawnAndStartMongoContainer finds an available port and launchs a mongo server docker container.
+// It returns the mongoURI, the port the mongo service is hosted on.
+// This must be called after initDocker.
+func (testConn *TestConnection) spawnAndStartMongoContainer(initTLS bool) (err error) {
+	testConn.portNumber, err = GetAvailablePort()
+	if err != nil {
+		testConn.logger.WithField("err", err).Error("No ports were available to bind the test docker mongo container to")
+		return ErrNoAvailablePorts
+	}
+	// TODO: Consider using different error types for these returns
+	testConn.mongoContainerID, err = testConn.startMongoContainer(testConn.mongoVersion, testConn.portNumber, initTLS)
+	if err != nil {
+		testConn.logger.WithField("err", err).Error("Could not spawn the to mongo container")
+		return err
+	}
+	testConn.mongoURI = fmt.Sprintf("mongodb://127.0.0.1:%d", testConn.portNumber)
+	// TODO: Add container to a global connection pool - ensure the connection pool
+	// is reaped when existing unless DisableContainerReaping is enabled.
+	return nil
 }
 
 // NewTestConnection is the standard method for initializing a TestConnection - it has a side-effect
@@ -42,47 +85,35 @@ type TestConnection struct {
 func NewTestConnection(spinupDockerContainer bool) (*TestConnection, error) {
 	// TODO: How should we be handling logging? What do other libraries typically do?
 	logger := logrus.New().WithField("src", "mongotest.TestConnection")
-	mongoURI := "mongodb://127.0.0.1"
 	testConn := &TestConnection{
-		logger: logger,
+		logger:       logger,
+		mongoVersion: "latest",
 	}
 	defer func() {
 		if err := recover(); err != nil {
 			logger.WithFields(logrus.Fields{
 				"err":   err,
 				"stack": string(debug.Stack()),
-			}).Error("A panic occurred when trying to initialize a TestConnection")
+			}).Error("A panic occurred when trying to initialize a TestConnection - auto-destroying mongo container")
 			// Initialization crashed - ensure the mongo container is destroyed
 			_ = testConn.KillMongoContainer()
 		}
 	}()
+	testConn.initDocker()
 	if spinupDockerContainer {
-		dockerClient, err := docker.NewEnvClient()
+		initTLS := false
+		err := testConn.spawnAndStartMongoContainer(initTLS)
 		if err != nil {
-			testConn.logger.WithField("err", err).Error("Could not connect to docker daemon")
-			return testConn, ErrFailedToConnectToDockerDaemon
-		}
-		testConn.dockerClient = dockerClient
-		portNumber, err := GetAvailablePort()
-		if err != nil {
-			testConn.logger.WithField("err", err).Error("No ports were available to bind the test docker mongo container to")
-			return testConn, ErrNoAvailablePorts
-		}
-		// TODO: Consider using different error types for these returns
-		containerID, err := testConn.StartMongoContainer(portNumber)
-		if err != nil {
-			logger.WithField("err", err).Error("Could not spawn the to mongo container")
+			// Error logged already
 			return testConn, err
 		}
-		testConn.mongoContainerID = containerID
-		mongoURI = fmt.Sprintf("%s:%d", mongoURI, portNumber)
 	}
-	conn, err := easymongo.ConnectWith(mongoURI).Connect()
+	conn, err := easymongo.ConnectWith(testConn.mongoURI).Connect()
 	testConn.Connection = conn
 	if err != nil {
 		logger.WithFields(logrus.Fields{
 			"err":      err,
-			"mongoURI": mongoURI,
+			"mongoURI": testConn.mongoURI,
 		}).Error("Could not connect to mongo instance")
 		return testConn, err
 	}
@@ -105,7 +136,7 @@ func NewTestConnection(spinupDockerContainer bool) (*TestConnection, error) {
 	if err != nil {
 		logger.WithFields(logrus.Fields{
 			"err":      err,
-			"mongoURI": mongoURI,
+			"mongoURI": testConn.mongoURI,
 		}).Errorf("Could not ping the test mongo instance after %d checks", numChecks)
 		// Try to teardown the mongo container (it might not have started)
 		_ = testConn.KillMongoContainer()
@@ -157,10 +188,10 @@ func (tc *TestConnection) pullMongoContainer(mongoImageName string) (err error) 
 	// Pull the initial container
 	tc.logger.Info("Starting mongo docker image pull")
 	rc, err := tc.dockerClient.ImagePull(context.Background(), mongoImageName, types.ImagePullOptions{})
-	defer rc.Close()
 	if err != nil {
 		return fmt.Errorf("could not pull mongo container: %v", err)
 	}
+	defer rc.Close()
 	if _, err := ioutil.ReadAll(rc); err != nil {
 		return fmt.Errorf("could not pull mongo container: %v", err)
 	}
@@ -168,43 +199,88 @@ func (tc *TestConnection) pullMongoContainer(mongoImageName string) (err error) 
 	return nil
 }
 
-// StartMongoContainer starts a mongo docker container
-// A note that the docker daemon on the system is expected to be running
-// TODO: Is there a way to spawn the docker daemon myself?
-func (tc *TestConnection) StartMongoContainer(portNumber int) (containerID string, err error) {
-	if len(tc.mongoContainerID) != 0 {
-		return "", ErrMongoContainerAlreadyRunning
-	}
-	pName := fmt.Sprintf("%d/tcp", portNumber)
-	containerName := fmt.Sprintf("mongo-%d", portNumber)
-
-	mongoImageName := "registry.hub.docker.com/library/mongo:latest"
-
-	containerResp, err := tc.dockerClient.ContainerCreate(
-		context.Background(),
-		&container.Config{
-			// TODO: Allow setting mongo version
-			Image: mongoImageName,
-			Labels: map[string]string{
-				"mongotest": "regression",
-			},
-			Tty:       true,
-			OpenStdin: true,
-			ExposedPorts: nat.PortSet{
-				nat.Port(pName): {},
-			},
+func containerConfig(mongoImageName, portName string, useTLS bool) *container.Config {
+	conf := &container.Config{
+		Image: mongoImageName,
+		Labels: map[string]string{
+			"mongotest": "regression",
 		},
-		&container.HostConfig{
-			PortBindings: nat.PortMap{
-				nat.Port("27017/tcp"): []nat.PortBinding{
-					{
-						HostIP:   "127.0.0.1",
-						HostPort: pName,
-					},
+		Tty:       true,
+		OpenStdin: true,
+		ExposedPorts: nat.PortSet{
+			nat.Port(portName): {},
+		},
+	}
+	if useTLS {
+		// These flags are based on this docker run command:
+		// docker run -d -v /path/to/pem/:/etc/ssl/ mongo:3.6 --sslMode requireSSL --sslPEMKeyFile /etc/ssl/mongodb.pem <additional options>
+		// TODO: Figure out how to mount a volume properly - may be right without this map
+		// -v /path/to/pem/:/etc/ssl/
+		// conf.Volumes = map[string]struct{}{}
+		conf.Cmd = []string{"--sslMode", "requireSSL", "--sslPEMKeyFile", "/etc/ssl/mongodb.pem"}
+	}
+	return conf
+}
+
+// These flags are based on this docker run command:
+// docker run -d -v /path/to/pem/:/etc/ssl/ mongo:3.6 --sslMode requireSSL --sslPEMKeyFile /etc/ssl/mongodb.pem <additional options>
+func dockerHostConfigWithTLS(portName string) (conf *container.HostConfig, caPemFile *os.File) {
+	// Get the default dockerHostConfig
+	conf = dockerHostConfig(portName)
+	// Write out
+	caPemFile, err := ioutil.TempFile(os.TempDir(), "mongo-tls-")
+	if err != nil {
+		panic(fmt.Errorf("could not create temporary file during testing: %w", err))
+	}
+	_, pemFile, _ := GenerateCARoot()
+	_, err = caPemFile.Write(pemFile)
+	if err != nil {
+		panic(fmt.Errorf("could not write cert to temporary file during testing: %w", err))
+	}
+	conf.Mounts = []mount.Mount{{
+		Type: mount.TypeBind,
+		// Source is the host path - point at the CA cert that was just generated
+		Source: caPemFile.Name(),
+		// Target is the path inside docker - technically the recommended command mounts
+		// the whole directory, but this should work
+		Target: "/etc/ssl/mongodb.pem",
+	}}
+	return conf, caPemFile
+}
+
+func dockerHostConfig(portName string) *container.HostConfig {
+	conf := &container.HostConfig{
+		PortBindings: nat.PortMap{
+			nat.Port("27017/tcp"): []nat.PortBinding{
+				{
+					HostIP:   "127.0.0.1",
+					HostPort: portName,
 				},
 			},
 		},
-		// TODO: Does this config also need to be specified?
+	}
+
+	return conf
+}
+
+// startMongoContainer starts a mongo docker container
+// A note that the docker daemon on the system is expected to be running
+// TODO: Is there a way to spawn the docker daemon myself?
+func (tc *TestConnection) startMongoContainer(mongoVersion string, portNumber int, initTLS bool) (containerID string, err error) {
+	if len(tc.mongoContainerID) != 0 {
+		return "", ErrMongoContainerAlreadyRunning
+	}
+	portName := fmt.Sprintf("%d/tcp", portNumber)
+	containerName := fmt.Sprintf("mongo-%d", portNumber)
+	mongoImageName := "registry.hub.docker.com/library/mongo:" + mongoVersion
+	hostConf := dockerHostConfig(portName)
+	if initTLS {
+		hostConf, tc.caPemFile = dockerHostConfigWithTLS(portName)
+	}
+	containerResp, err := tc.dockerClient.ContainerCreate(
+		context.Background(),
+		containerConfig(mongoImageName, portName, initTLS),
+		hostConf,
 		&network.NetworkingConfig{},
 		containerName)
 	if err != nil && docker.IsErrNotFound(err) {
@@ -215,9 +291,9 @@ func (tc *TestConnection) StartMongoContainer(portNumber int) (containerID strin
 			return "", err
 		}
 		// Now that the pull is complete, we can try to call start again
-		return tc.StartMongoContainer(portNumber)
+		return tc.startMongoContainer(mongoVersion, portNumber, initTLS)
 	} else if err != nil {
-		tc.logger.WithField("err", err).Error("Could not create the docker container")
+		tc.logger.WithField("err", err).Error("Could not start the docker container")
 		return "", err
 	}
 	containerID = containerResp.ID
@@ -252,6 +328,16 @@ func (tc *TestConnection) KillMongoContainer() (err error) {
 		// No container was ever launched, nothing to be done
 		return nil
 	}
+	if tc.caPemFile != nil {
+		// If a tmp CA pem file was written out to the OS, attempt to clean it up
+		err = os.Remove(tc.caPemFile.Name())
+		tc.logger.WithFields(logrus.Fields{
+			"err":         err,
+			"containerID": tc.caPemFile.Name(),
+		}).Error("Could not delete generated CA PEM temporary file - still will attempt to teardown docker container...")
+		err = nil
+		tc.caPemFile = nil
+	} // Note that we do not error out if we couldn't clean-up the temporary file
 	err = tc.dockerClient.ContainerRemove(context.Background(),
 		tc.mongoContainerID,
 		types.ContainerRemoveOptions{
@@ -272,4 +358,37 @@ func (tc *TestConnection) KillMongoContainer() (err error) {
 	return nil
 }
 
+// EasyMongoWithContainer spawns a docker container on an available port,
+// connects to the mongo database, runs the provided function,
+// then kills the mongo container as it exits.
+// A note that the function isn't actually executed inside the container, instead
+// a connection is established to the mongo server from the host system.
+func EasyMongoWithContainer(f func(c *easymongo.Connection) error) (err error) {
+	spinupDockerContainer := true
+	tc, err := NewTestConnection(spinupDockerContainer)
+	if err != nil {
+		return err
+	}
+	defer tc.KillMongoContainer()
+	// Run whatever function it is
+	return f(tc.Connection)
+}
+
+// MongoClientWithContainer spawns a docker container on an available port,
+// connects to the mongo database, runs the provided function,
+// then kills the mongo container as it exits.
+// A note that the function isn't actually executed inside the container, instead
+// a connection is established to the mongo server from the host system.
+func MongoClientWithContainer(f func(m *mongo.Client) error) error {
+	spinupDockerContainer := true
+	tc, err := NewTestConnection(spinupDockerContainer)
+	if err != nil {
+		return err
+	}
+	defer tc.KillMongoContainer()
+	// Run whatever function it is using the mongo driver connection
+	return f(tc.Connection.MongoDriverClient())
+}
+
 // TODO: DropAllDatabases
+// TODO
